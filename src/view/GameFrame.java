11b954758677f8c28af85e3d78034aaf23f2173b
josefir98 <55584458+josefir98@users.noreply.github.com>
2022-01-06 06:22:47 +0100

    Added fullscreen and some menu bar shortcuts.

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -4,10 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Enumeration;
 
 public class GameFrame extends JFrame implements ActionListener {
 
     SnakeMenuBar menuBar;
+    JRadioButtonMenuItem previousRes;
+    boolean fullScreen = false;
 
     //System.out.println();
     GamePanel gamePanel;
@@ -17,23 +20,14 @@ public class GameFrame extends JFrame implements ActionListener {
     public GameFrame(int width, int height) {
 
         // Setup menu bar
-        this.menuBar = new SnakeMenuBar();
-        this.menuBar.exitItem.addActionListener(this);
-        this.menuBar.resolutionItem.addActionListener(this);
-        this.menuBar.r480pItem.addActionListener(this);
-        this.menuBar.r720pItem.addActionListener(this);
-        this.menuBar.r1080pItem.addActionListener(this);
-        this.menuBar.r1440pItem.addActionListener(this);
-        this.menuBar.fullScreenItem.addActionListener(this);
-        this.setJMenuBar(menuBar);
-
+        setupMenuBar();
         // Setup game panel
-        this.width = width;
-        this.height = height;
-        this.gamePanel = new GamePanel(width, height);
-        this.add(gamePanel);
-
+        setupGamePanel(width, height);
         // Setup game frame
+        setupGameFrame();
+    }
+
+    private void setupGameFrame() {
         this.setTitle("Twisted Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
@@ -44,12 +38,73 @@ public class GameFrame extends JFrame implements ActionListener {
         this.setVisible(true);
     }
 
+    private void setupGamePanel(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.gamePanel = new GamePanel(width, height);
+        this.add(gamePanel);
+    }
+
+    private void setupMenuBar() {
+        this.menuBar = new SnakeMenuBar();
+
+        this.menuBar.exitItem.addActionListener(this);
+
+        this.menuBar.fullScreenItem.addActionListener(this);
+        this.menuBar.r480pItem.addActionListener(this);
+        this.menuBar.r480pItem.setSelected(true);
+        this.menuBar.r720pItem.addActionListener(this);
+        this.menuBar.r1080pItem.addActionListener(this);
+        this.menuBar.r1440pItem.addActionListener(this);
+        this.menuBar.r100sItem.addActionListener(this);
+        this.menuBar.r200sItem.addActionListener(this);
+        this.menuBar.r300sItem.addActionListener(this);
+        this.menuBar.r400sItem.addActionListener(this);
+        this.menuBar.r500sItem.addActionListener(this);
+        this.menuBar.r600sItem.addActionListener(this);
+        this.menuBar.r700sItem.addActionListener(this);
+        this.menuBar.r800sItem.addActionListener(this);
+        this.menuBar.r900sItem.addActionListener(this);
+        this.menuBar.r1000sItem.addActionListener(this);
+
+        this.setJMenuBar(menuBar);
+    }
+
+    private AbstractButton getSelectedButton(ButtonGroup buttonGroup) {
+        for (Enumeration<AbstractButton> buttons = buttonGroup.getElements(); buttons.hasMoreElements();) {
+            AbstractButton button = buttons.nextElement();
+            if (button.isSelected()) {
+                return button;
+            }
+        }
+        return null;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Exit
         if (e.getSource() == menuBar.exitItem) {
             System.exit(0);
         }
-
+        // Fullscreen
+        if (e.getSource() == menuBar.fullScreenItem) {
+            if (fullScreen) {
+                previousRes.doClick();
+                this.setLocationRelativeTo(null);
+                fullScreen = false;
+            } else {
+                previousRes = (JRadioButtonMenuItem) getSelectedButton(menuBar.resolutionGroup);
+                this.dispose();
+                this.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                this.setUndecorated(true);
+                this.setVisible(true);
+                this.width = this.getContentPane().getWidth();
+                this.height = this.getContentPane().getHeight();
+                //this.pack();
+                gamePanel.sizeAdjust(width, height);
+                fullScreen = true;
+            }
+        }
         // 640 x 480   (480p)
         if (e.getSource() == menuBar.r480pItem) {
             this.width = 640;
@@ -89,6 +144,106 @@ public class GameFrame extends JFrame implements ActionListener {
             this.getContentPane().setPreferredSize(new Dimension(width, height));
             this.pack();
             gamePanel.sizeAdjust(2560, 1440);
+            this.setVisible(true);
+        }
+        // 100 x 100
+        if (e.getSource() == menuBar.r100sItem) {
+            this.width = 100;
+            this.height = 100;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(100, 100);
+            this.setVisible(true);
+        }
+        // 200 x 200
+        if (e.getSource() == menuBar.r200sItem) {
+            this.width = 200;
+            this.height = 200;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(200, 200);
+            this.setVisible(true);
+        }
+        // 300 x 300
+        if (e.getSource() == menuBar.r300sItem) {
+            this.width = 300;
+            this.height = 300;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(300, 300);
+            this.setVisible(true);
+        }
+        // 400 x 400
+        if (e.getSource() == menuBar.r400sItem) {
+            this.width = 400;
+            this.height = 400;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(400, 400);
+            this.setVisible(true);
+        }
+        // 500 x 500
+        if (e.getSource() == menuBar.r500sItem) {
+            this.width = 500;
+            this.height = 500;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(500, 500);
+            this.setVisible(true);
+        }
+        // 600 x 600
+        if (e.getSource() == menuBar.r600sItem) {
+            this.width = 600;
+            this.height = 600;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(600, 600);
+            this.setVisible(true);
+        }
+        // 700 x 700
+        if (e.getSource() == menuBar.r700sItem) {
+            this.width = 700;
+            this.height = 700;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(700, 700);
+            this.setVisible(true);
+        }
+        // 800 x 800
+        if (e.getSource() == menuBar.r800sItem) {
+            this.width = 800;
+            this.height = 800;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(800, 800);
+            this.setVisible(true);
+        }
+        // 900 x 900
+        if (e.getSource() == menuBar.r900sItem) {
+            this.width = 900;
+            this.height = 900;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(900, 900);
+            this.setVisible(true);
+        }
+        // 1000 x 1000
+        if (e.getSource() == menuBar.r1000sItem) {
+            this.width = 1000;
+            this.height = 1000;
+            this.dispose();
+            this.getContentPane().setPreferredSize(new Dimension(width, height));
+            this.pack();
+            gamePanel.sizeAdjust(1000, 1000);
             this.setVisible(true);
         }
     }
--- src/view/SnakeMenuBar.java
+++ src/view/SnakeMenuBar.java
@@ -1,6 +1,8 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class SnakeMenuBar extends JMenuBar {
 
@@ -9,11 +11,22 @@ public class SnakeMenuBar extends JMenuBar {
 
     public JMenu displayMenu;
     public JMenu resolutionItem;
-    public JMenuItem r480pItem;
-    public JMenuItem r720pItem;
-    public JMenuItem r1080pItem;
-    public JMenuItem r1440pItem;
+    public ButtonGroup resolutionGroup;
     public JMenuItem fullScreenItem;
+    public JRadioButtonMenuItem r480pItem;
+    public JRadioButtonMenuItem r720pItem;
+    public JRadioButtonMenuItem r1080pItem;
+    public JRadioButtonMenuItem r1440pItem;
+    public JRadioButtonMenuItem r100sItem;
+    public JRadioButtonMenuItem r200sItem;
+    public JRadioButtonMenuItem r300sItem;
+    public JRadioButtonMenuItem r400sItem;
+    public JRadioButtonMenuItem r500sItem;
+    public JRadioButtonMenuItem r600sItem;
+    public JRadioButtonMenuItem r700sItem;
+    public JRadioButtonMenuItem r800sItem;
+    public JRadioButtonMenuItem r900sItem;
+    public JRadioButtonMenuItem r1000sItem;
 
     public JMenu soundMenu;
     public JMenuItem muteItem;
@@ -23,30 +36,72 @@ public class SnakeMenuBar extends JMenuBar {
         // Setup File
         fileMenu = new JMenu("File");
         exitItem = new JMenuItem("Exit");
+        exitItem.setAccelerator(KeyStroke.getKeyStroke("control Q"));
         this.add(fileMenu);
         fileMenu.add(exitItem);
+
 
         // Setup Display
         displayMenu = new JMenu("Display");
         resolutionItem = new JMenu("Resolution");
-        r480pItem = new JMenuItem("640 x 480        (480p)");
-        r720pItem = new JMenuItem("1280 x 720      (720p)");
-        r1080pItem = new JMenuItem("1920 x 1080    (1080p)");
-        r1440pItem = new JMenuItem("2560 x 1440    (1440p)");
         fullScreenItem = new JMenuItem("Fullscreen");
+        fullScreenItem.setAccelerator(KeyStroke.getKeyStroke("control F"));
+        r480pItem = new JRadioButtonMenuItem("640 x 480");
+        r720pItem = new JRadioButtonMenuItem("1280 x 720");
+        r1080pItem = new JRadioButtonMenuItem("1920 x 1080");
+        r1440pItem = new JRadioButtonMenuItem("2560 x 1440");
+        r100sItem = new JRadioButtonMenuItem("100 x 100");
+        r200sItem = new JRadioButtonMenuItem("200 x 200");
+        r300sItem = new JRadioButtonMenuItem("300 x 300");
+        r400sItem = new JRadioButtonMenuItem("400 x 400");
+        r500sItem = new JRadioButtonMenuItem("500 x 500");
+        r600sItem = new JRadioButtonMenuItem("600 x 600");
+        r700sItem = new JRadioButtonMenuItem("700 x 700");
+        r800sItem = new JRadioButtonMenuItem("800 x 800");
+        r900sItem = new JRadioButtonMenuItem("900 x 900");
+        r1000sItem = new JRadioButtonMenuItem("1000 x 1000)");
+
+        resolutionGroup = new ButtonGroup();
+        resolutionGroup.add(r480pItem);
+        resolutionGroup.add(r720pItem);
+        resolutionGroup.add(r1080pItem);
+        resolutionGroup.add(r1440pItem);
+        resolutionGroup.add(r100sItem);
+        resolutionGroup.add(r200sItem);
+        resolutionGroup.add(r300sItem);
+        resolutionGroup.add(r400sItem);
+        resolutionGroup.add(r500sItem);
+        resolutionGroup.add(r600sItem);
+        resolutionGroup.add(r700sItem);
+        resolutionGroup.add(r800sItem);
+        resolutionGroup.add(r900sItem);
+        resolutionGroup.add(r1000sItem);
+
         this.add(displayMenu);
+        displayMenu.add(fullScreenItem);
+        displayMenu.add(new JSeparator());
         displayMenu.add(resolutionItem);
         resolutionItem.add(r480pItem);
         resolutionItem.add(r720pItem);
         resolutionItem.add(r1080pItem);
         resolutionItem.add(r1440pItem);
-        displayMenu.add(fullScreenItem);
+        resolutionItem.add(new JSeparator());
+        resolutionItem.add(r100sItem);
+        resolutionItem.add(r200sItem);
+        resolutionItem.add(r300sItem);
+        resolutionItem.add(r400sItem);
+        resolutionItem.add(r500sItem);
+        resolutionItem.add(r600sItem);
+        resolutionItem.add(r700sItem);
+        resolutionItem.add(r800sItem);
+        resolutionItem.add(r900sItem);
+        resolutionItem.add(r1000sItem);
 
         // Setup Sound
         soundMenu = new JMenu("Sound");
         muteItem = new JMenuItem("Toggle Mute");
         this.add(soundMenu);
-        displayMenu.add(muteItem);
+        soundMenu.add(muteItem);
     }
 
 }
